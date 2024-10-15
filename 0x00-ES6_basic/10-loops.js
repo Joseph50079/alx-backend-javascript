14c5dd1a82ddb003e74669f73c985d591a716a22
Joseph50079 <josephogbah2019@gmail.com>
2024-10-15 18:32:50 +0100

    for...of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const idx in array) {
+  for (const idx of array) {
     if (Object.prototype.hasOwnProperty.call(array, idx)) {
       const value = array[idx];
       newArray.push(appendString + value);
