6e18c92a66fbd8dcaf85fcbeb24ad0362eef011e
Joseph50079 <josephogbah2019@gmail.com>
2024-10-15 18:21:56 +0100

    do and go

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx in array) {
+    if (Object.prototype.hasOwnProperty.call(array, idx)) {
+      const value = array[idx];
+      newArray.push(appendString + value);
+    }
+  }
+
+  return array;
+}
