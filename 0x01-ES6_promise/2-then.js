d6ddd1ae962b0bd6da45b2bff2e43ef764460f9e
Joseph50079 <josephogbah2019@gmail.com>
2024-11-27 12:43:01 +0100

    done then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
     body: 'success',
@@ -6,6 +6,3 @@ function handleResponseFromAPI(promise) {
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
-
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
