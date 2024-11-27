038cf911c7280e2b273f1f2bb9f68f6ef39534e0
Joseph50079 <josephogbah2019@gmail.com>
2024-11-27 13:54:46 +0100

    fixed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,9 @@
 import { createUser, uploadPhoto } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       const [photo, user] = result;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     }).catch(() => new Error('Signup system offline'));
 }
-
-handleProfileSignup();
